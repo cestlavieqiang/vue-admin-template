078d51e03f4652bc3d0d9cf41db728e1028d206e
shizhiqiang <shizhiqiang@noxgroup.com>
2023-04-21 17:57:23 +0800

    新增弹框;

--- src/views/public/department.vue
+++ src/views/public/department.vue
@@ -45,13 +45,34 @@
             {{ scope.row.terminal }}
           </template>
         </el-table-column>
-        <el-table-column align="更改时间" prop="created_at" label="Display_time">
+        <el-table-column align="center" prop="created_at" label="更改时间">
           <template slot-scope="scope">
             <i class="el-icon-time" />
             <span>{{ scope.row.updateTime }}</span>
           </template>
         </el-table-column>
       </el-table>
+    <!-- 新增项目界面 -->
+    <el-dialog title="新增项目" :visible.sync="projectVisible" width="600px" destroy-on-close>
+        <el-form label-width="120px" style="padding-right: 30px;" :model="projectForm" label-position="top" :rules="rules" ref="projectForm">
+          <el-form-item label="部门名称" prop="projectAdmin">
+                <el-select size="small" style="width: 100%" v-model="projectForm.projectAdmin" filterable placeholder="请选择项目管理员">
+                    <el-option v-for="item in userList" :key="item.id" :label="item.username" :value="item.id"/>
+                </el-select>
+          </el-form-item>  
+          <el-form-item label="项目名称" prop="name">
+                <el-input size="small" style="width: 100%" v-model="projectForm.name" placeholder="请输入项目名称"/>
+          </el-form-item>
+          <el-form-item label="项目描述" prop="description">
+                <el-input size="small" style="width: 100%" :autosize="{ minRows: 4}" type="textarea" clearable placeholder="请输入项目描述" 
+                        v-model="projectForm.description" maxlength="200" show-word-limit/>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button size="small" @click="projectVisible=false">取消</el-button>
+            <el-button size="small" type="primary" @click="submitProjectForm('projectForm', projectForm)">确定</el-button>
+        </div>
+    </el-dialog>
     </div>
   </template>
 
@@ -71,8 +92,10 @@ export default {
   },
   data() {
     return {
+      projectVisible: false,
       list: null,
       listLoading: true,
+      projectForm: {},
       searchForm: {
                 page: 1,
                 limit: 10,
@@ -101,9 +124,11 @@ export default {
             this.searchForm.condition = "";
             this.fetchData(this.searchForm);
         },
+        // 新增项目
         addProject(){
-
-        }
+            this.projectForm = {};
+            this.projectVisible = true;
+        },
 
   }
 }
